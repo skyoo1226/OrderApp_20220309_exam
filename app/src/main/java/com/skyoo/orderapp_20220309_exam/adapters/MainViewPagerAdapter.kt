6e89ccc3280f0e6bdc87b71d492f0e3e1dcb1505
skyoo1226 <you1226@naver.com>
2022-03-09 20:30:57 +0900

    뷰페이저에서 getCount()함수와 getItem()함수를 구현 함

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/MainViewPagerAdapter.kt
@@ -7,8 +7,11 @@ import com.skyoo.orderapp_20220309_exam.fragments.MyProfileFragment
 import com.skyoo.orderapp_20220309_exam.fragments.PiazzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager ) : FragmentPagerAdapter(fm) {
+
+//    구현 할 총 페이지가 2개이므로 getCount()는 2개로 단순하게 처리
     override fun getCount() = 2
 
+//    getItem()은 2가지 경우 임. '0'과 'else'롤 각각 어떤 프래그먼트로 갈지 정해 줌.
     override fun getItem(position: Int): Fragment {
 
         return when(position) {
