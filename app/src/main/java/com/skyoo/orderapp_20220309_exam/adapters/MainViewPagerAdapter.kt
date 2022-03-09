8946a06aadf7639ceb111396f76187e9b36cfb48
skyoo1226 <you1226@naver.com>
2022-03-09 20:28:48 +0900

    뷰페이저에서 getCount()함수와 getItem()함수를 구현 함

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/MainViewPagerAdapter.kt
@@ -3,13 +3,18 @@ package com.skyoo.orderapp_20220309_exam.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.skyoo.orderapp_20220309_exam.fragments.MyProfileFragment
+import com.skyoo.orderapp_20220309_exam.fragments.PiazzaStoreListFragment
 
 class MainViewPagerAdapter(fm: FragmentManager ) : FragmentPagerAdapter(fm) {
-    override fun getCount(): Int {
-
-    }
+    override fun getCount() = 2
 
     override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> PiazzaStoreListFragment()
+            else -> MyProfileFragment()
+        }
 
     }
 }
