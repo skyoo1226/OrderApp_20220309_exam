b76f6aa26ec3197b977096bac7c67796212e792b
skyoo1226 <you1226@naver.com>
2022-03-09 21:31:23 +0900

    메인 액티비티에서 메인 뷰페이저를 mAdater로 연결. -앱에서 '피자가게목록'과 '내정보표시' 화면만 좌우로 움직이게 함.

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/MainActivity.kt
@@ -2,10 +2,25 @@ package com.skyoo.orderapp_20220309_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skyoo.orderapp_20220309_exam.adapters.MainViewPagerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+// mAdapter는 나중에 (객체화)대입한다고 lateinit var 변수로 지정, 그래서 setValues에서 씀.
+    lateinit var mAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+    fun setupEvents() {
+
+    }
+    fun setValues() {
+        mAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mAdapter
+
     }
 }
