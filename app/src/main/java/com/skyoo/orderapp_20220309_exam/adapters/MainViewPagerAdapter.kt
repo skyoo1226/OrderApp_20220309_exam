c10f166310804e9297e4f8fa871997a1542e540c
skyoo1226 <you1226@naver.com>
2022-03-09 20:18:48 +0900

    뷰페이저 어댑터를 만들어 - 상속과 오버라이딩 문제를 해결 - 각 함수 내용 구현 해야함 getCount(), getItem()

---
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,15 @@
+package com.skyoo.orderapp_20220309_exam.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdapter(fm: FragmentManager ) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
+}
