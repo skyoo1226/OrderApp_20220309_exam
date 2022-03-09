7243aa08b07027f49a34c11a615290aa5aa2facb
skyoo1226 <you1226@naver.com>
2022-03-10 02:46:31 +0900

    getView에서 데이터 UI를 추출해서 우선 이름만 연결

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/StoreAdapter.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/StoreAdapter.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import com.skyoo.orderapp_20220309_exam.R
 import com.skyoo.orderapp_20220309_exam.datas.StoreData
+import com.willy.ratingbar.BaseRatingBar
+import de.hdodenhof.circleimageview.CircleImageView
 
 class StoreAdapter(
     val mContext: Context,      // 어느화면에서 쓸지
@@ -22,6 +26,17 @@ class StoreAdapter(
             tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
         }
         val row = tempRow!!
+//    StoreData의 mList(가게이름)를 data에 주고, store_list_item.xml에서 각 아이템마다 id를 지정해 준다.
+        val data = mList[position]
+//  바로 위 data들을 가져 올 UI들을 지정해 줌
+//  이렇게 쓰는데  써클이미지뷰는 가용성이 있어 간단히 아래처럼 써도 됨. val imgLogo = row.findViewById<CircleImageView>(R.id.imgLogo)
+        val imLogo = row.findViewById<ImageView>(R.id.imgLogo)
+        val txtName = row.findViewById<TextView>(R.id.txtName)
+        val scoreRatingBar = row.findViewById<BaseRatingBar>(R.id.scoreRatingBar)
+
+        txtName.text = data.name
+
+
         return row
 
     }
