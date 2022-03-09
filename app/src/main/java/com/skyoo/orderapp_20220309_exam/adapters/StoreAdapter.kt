a5d8c9bec1f5586369fe17633dfe4cad33f94b6d
skyoo1226 <you1226@naver.com>
2022-03-10 01:33:15 +0900

    어댑터의 getView 함수 기초 작업

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/StoreAdapter.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/adapters/StoreAdapter.kt
@@ -1,7 +1,11 @@
 package com.skyoo.orderapp_20220309_exam.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.skyoo.orderapp_20220309_exam.R
 import com.skyoo.orderapp_20220309_exam.datas.StoreData
 
 class StoreAdapter(
@@ -10,4 +14,16 @@ class StoreAdapter(
     val resId: Int,
     val mList: ArrayList<StoreData>
 ) : ArrayAdapter<StoreData>(mContext, resId, mList) {
+
+//    getView로 layout.store_list_item을 불러 옴. tempRow를 이용 convertView에 뭐가 되었던...
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.store_list_item, null)
+        }
+        val row = tempRow!!
+    return row
+
+    }
 }
