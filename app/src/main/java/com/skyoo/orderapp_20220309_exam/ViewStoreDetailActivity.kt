0e956e4d3876ed7800b7463c6ab3e70e7b530f81
skyoo1226 <you1226@naver.com>
2022-03-12 10:57:47 +0900

    피자주문 왼쪽 화면만 완성

--- app/src/main/java/com/skyoo/orderapp_20220309_exam/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/skyoo/orderapp_20220309_exam/ViewStoreDetailActivity.kt
@@ -5,21 +5,50 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class ViewStoreDetailActivity : AppCompatActivity() {
+    lateinit var mStoreData: StoreData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_store_detail)
-        
+        mStoreData = intent.getSerializableExtra("store") as StoreData
         setupEvents()
         setValues()
     }
 
     fun setupEvents() {
 
+        btnCall.setOnClickListener {
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+                    val myUri = Uri.parse("tel:${mStoreData.phoneNum}")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@ViewStoreDetailActivity,  "전화 연결을 하려면, 통화 권한이 필요합니다.", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions( Manifest.permission.CALL_PHONE )
+                .check()
+
+        }
+
     }
 
     fun setValues() {
-
-
+        txtName.text = mStoreData.name
+        Glide.with(this).load(mStoreData.logoUrl).into(imgLogo)
+        txtPhoneNum.text = mStoreData.phoneNum
+        scoreRatingBar.rating = mStoreData.score.toFloat()
+        txtScore.text = "(${mStoreData.score})"
     }
-
 }
